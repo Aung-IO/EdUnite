6eaa2205cb6733d5d217816df58c5f7900c19003
Aung Pyae Kyaw <aungpyaekyawwiz242@gmail.com>
2025-04-27 01:31:33 +0630

    link test

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
 import router from "./routes/router.tsx";
 import { AnimatePresence } from "framer-motion";
 
@@ -9,11 +9,13 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
+  <BrowserRouter>
     <QueryClientProvider client={queryClient}>
       <AnimatePresence mode="wait">
 
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
       </AnimatePresence>
     </QueryClientProvider>
+  </BrowserRouter>
   // </StrictMode>
 );
